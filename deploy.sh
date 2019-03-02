baf5cc172a9dc99ea26fe9befa6d530efd672597
stozuka <stozuka@gmail.com>
2019-03-02 14:30:14 +0800

    add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker container build stozuka/multi-client:latest -t stozuka/multi-client:$SHA -f ./client/Dockerfile.dev ./client
+docker container build stozuka/multi-server:latest -t stozuka/multi-server:$SHA -f ./server/Dockerfile.dev ./server
+docker container build stozuka/multi-worker:latest -t stozuka/multi-worker:$SHA -f ./worker/Dockerfile.dev ./worker
+docker push stozuka/multi-client:latest
+docker push stozuka/multi-server:latest
+docker push stozuka/multi-worker:latest
+docker push stozuka/multi-client:$SHA
+docker push stozuka/multi-server:$SHA
+docker push stozuka/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stozuka/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stozuka/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stozuka/multi-worker:$SHA
